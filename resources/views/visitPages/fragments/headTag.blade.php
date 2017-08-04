3143a152445bbde59f0098a38c9d70738f7521cb
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2017-08-04 18:40:10 -0500

    refactor(visitPages): use bootstrap.min.css

--- resources/views/visitPages/fragments/headTag.blade.php
+++ resources/views/visitPages/fragments/headTag.blade.php
@@ -1,5 +1,5 @@
         <!-- Bootstrap -->
-        <link href="{{asset('css/bootstrap.css')}}" rel="stylesheet" />
+        <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" />
         <link rel="stylesheet" type="text/css"	href="{{asset('fonts/font-awesome/css/font-awesome.css')}}" />
         
         <!-- Stylesheet
