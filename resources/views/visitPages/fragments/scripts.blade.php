7d9913ed4d7aef919a96de7247507f8ee197a08f
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2017-08-04 19:38:02 -0500

    refactor(js): remove unused code

--- resources/views/visitPages/fragments/scripts.blade.php
+++ resources/views/visitPages/fragments/scripts.blade.php
@@ -1,11 +1,7 @@
-    	<script type="text/javascript" src="{{asset('js/jquery.1.11.1.js')}}"></script>
+	<script type="text/javascript" src="{{asset('js/jquery.1.11.1.js')}}"></script>
 	<script type="text/javascript" src="{{asset('js/jquery.js')}}"></script>
 	<script type="text/javascript" src="{{asset('js/bootstrap.min.js')}}"></script>
 	<script type="text/javascript" src="{{asset('js/jquery.prettyPhoto.js')}}"></script>
-	<script type="text/javascript" src="{{asset('js/jquery.counterup.js')}}"></script>
-	<script type="text/javascript" src="{{asset('js/waypoints.js')}}"></script>
-	<script type="text/javascript" src="{{asset('js/owl.carousel.js')}}"></script>
 	<script type="text/javascript" src="{{asset('js/main.js')}}"></script>
 	
-	<script type="text/javascript" src="{{asset('js/easypiechart.js')}}"></script>
 	<script type="text/javascript" src="{{asset('js/jquery.isotope.js')}}"></script>
