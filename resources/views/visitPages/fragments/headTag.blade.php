8891c80f9212ec6541b35be404a928fbc2747533
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2017-08-04 19:19:12 -0500

    fix: events not working

--- resources/views/visitPages/fragments/headTag.blade.php
+++ resources/views/visitPages/fragments/headTag.blade.php
@@ -1,11 +1,12 @@
         <!-- Bootstrap -->
         <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet" />
         <link rel="stylesheet" type="text/css"	href="{{asset('fonts/font-awesome/css/font-awesome.css')}}" />
-        
+
         <!-- Stylesheet
             ================================================== -->
         <link rel="stylesheet" type="text/css"	href="{{asset('css/style.css')}}" />
         <link rel="stylesheet" type="text/css"	href="{{asset('css/style2.css')}}" />
+        <link rel="stylesheet" type="text/css"	href="{{asset('css/prettyPhoto.css')}}" />
         
         <!-- FAVICON================================================= -->
         <link href="{{asset('img/favicon.ico')}}" rel="shortcut icon" type="image/x-icon">
