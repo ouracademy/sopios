d8cb9a65f4b1c6330c6c66202369d0448674f144
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2017-08-12 15:06:11 -0500

    chore(fragments): update what-is to add FCM

--- resources/views/copios/fragments/what-is.blade.php
+++ resources/views/copios/fragments/what-is.blade.php
@@ -3,12 +3,15 @@
     <h2>COPIOS 2017</h2>
     <div class="row">
       <div class="col-xs-12 col-md-12">
-        <p><a href="{{url("/eventos")}}" target="_blank">COPIOS, el Congreso Peruano de Investigación de Operaciones y de Sistemas</a>
-          desarrollado por la <a href="{{url("/")}}" target="_blank">Sociedad Peruana de Investigación Operativa y de Sistemas (SOPIOS)</a>,
-          es el principal congreso peruano de investigación operativa, sistemas y áreas afines, busca proveer un espacio
-          de encuentro y discusión de investigadores, profesionales y educadores del Perú y del extranjero, y con ello promover
-          el desarrollo de estas áreas, su aplicación en la industria, los servicios y el gobierno, y contribuir al desarrollo
-          científico y/o tecnológico del país.</p>
+        <p>
+          <a href="{{url("/eventos")}}" target="_blank">COPIOS, el Congreso Peruano de Investigación de Operaciones y de Sistemas</a> 
+          desarrollado por la <a href="{{url("/")}}" target="_blank">Sociedad Peruana de Investigación Operativa y de Sistemas (SOPIOS)</a>
+          y en esta edición junto a la <a href="http://matematicas.unmsm.edu.pe/pregrado/eap_operativa.php" target="_blank">Escuela Profesional
+          de Investigación Operativa de la Facultad de Ciencias Matemáticasde la Universidad Nacional Mayor San Marcos</a> 
+          es el principal congreso peruano de investigación operativa, sistemas y áreas afines, busca proveer
+          un espacio de encuentro y discusión de investigadores, profesionales y educadores del Perú y del extranjero, y
+          con ello promover el desarrollo de estas áreas, su aplicación en la industria, los servicios y el gobierno, y contribuir
+          al desarrollo científico y/o tecnológico del país.</p>
       </div>
     </div>
   </div>
