a8c3863cd1fb9d34e61bcf308f84603e70cbb73d
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2017-09-11 16:59:48 -0500

    styles(errors): improve 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,28 +1,22 @@
 @extends('visitPages.layout')
+@section('title', 'Página no encontrada')
 @section('content')
-		<!-- PAGE -->
-		<section id="page">
-			<div class="row">
-				<div class="container">
-					<div class="col-md-12 not-found">
-<!-- 						<div class="error">404</div> -->
-					</div>
-					<div class="col-md-6 not-found">
-						<div class="content">
-							<h3>¿Te has perdido en la naturaleza?</h3>
-							<p>
-								Lo sentimos, pero la página que buscas no se ha encontrado<br />
-								Intente comprobar la URL para errores.
-							</p>
-						</div>
-					</div>
-					<div class="col-md-6">
-						<div class="section-title" id="404img">
-							<img src="{{asset('img/404img.jpg')}}" alt=""  class="img-responsive"/>
-						</div>
-					</div>
+	<section id="page">
+		<div class="container">
+			<div class="col-md-6 not-found">
+				<div class="content">
+					<h1>¿Te has perdido en la naturaleza?</h1>
+					<p>
+						Lo sentimos, pero la página que buscas no se ha encontrado<br />
+						Intente comprobar la URL para errores.
+					</p>
 				</div>
 			</div>
-		</section>
-		<!--/PAGE -->
+			<div class="col-md-6">
+				<div class="section-title" id="404img">
+					<img src="{{asset('img/404img.jpg')}}" alt=""  class="img-responsive"/>
+				</div>
+			</div>
+		</div>
+	</section>
 @stop
