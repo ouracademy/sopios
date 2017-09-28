668a24dc0141ec3566bb150c73fa75e079450b14
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2017-09-27 23:39:52 -0500

    chore(copios): update important dates

--- resources/views/copios/callForContributions.blade.php
+++ resources/views/copios/callForContributions.blade.php
@@ -43,7 +43,7 @@
                         <div class="event-name">Convocatoria</div>
                     </li>
                     <li>
-                        <div class="schedule-time">30/07 - 25/09</div>
+                        <div class="schedule-time">30/07 - 10/10</div>
                         <div class="event-name">Recepción de resúmenes</div>
                     </li>
                     <li>
@@ -55,11 +55,11 @@
                         <div class="event-name">Evaluación de resúmenes</div>
                     </li>
                     <li>
-                        <div class="schedule-time">15/08 - 27/09</div>
+                        <div class="schedule-time">12/10 - 27/09</div>
                         <div class="event-name">Notificación de aceptación de resúmenes</div>
                     </li>
                     <li>
-                        <div class="schedule-time">01/09 - 18/10</div>
+                        <div class="schedule-time">01/09 - 30/10</div>
                         <div class="event-name">Recepción de resúmenes extendidos (*)</div>
                     </li>
                     <li>
