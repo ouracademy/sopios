b0f454c962f5828f67f2d3a346740f6e8381802b
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2017-10-27 22:39:47 -0500

    feat(copios): update schedule

--- resources/views/copios/schedule.blade.php
+++ resources/views/copios/schedule.blade.php
@@ -6,21 +6,9 @@
 <section id="science-fair">
   <div class="container-fluid">
     <h2>Programa</h2>
-    <div class="speakers">
-      @foreach($days as $day)
-      <h3>{{$day->date}}</h3>
-      <table class="table">
-        <tbody>
-          @foreach($day->slots as $slot)
-          <tr>
-            <td>{{$slot->timePeriod}}</td>
-            <td>{{$slot->name}}</td>
-          </tr>
-          @endforeach
-        </tbody>
-      </table>
-      <br>
-      @endforeach
+    <img src="{{asset('copios/img/schedule.png')}}"/>
+    <div>
+      <a href="{{asset('copios/Ponencias COPIOS2017.pdf')}}" class="btn btn-primary" download>Ve las ponencias detalladas aquí!</a>
     </div>
   </div>
 </section>
